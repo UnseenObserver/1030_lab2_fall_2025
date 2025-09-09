c773ee10db240c963aced89a4326587bb49e848d
Charlotte Pawloski <143750577+UnseenObserver@users.noreply.github.com>
2025-09-09 12:06:51 +0000

    First Commit

---
+++ EECS1030_lab2/EECS1020_lab2_Pawloski.java
@@ -0,0 +1,11 @@
+//This was created by Charlotte H. Pawloski on September 9, 2025
+
+package EECS1030_lab2_Pawloski;
+
+public class EECS1020_lab2_Pawloski {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+}
